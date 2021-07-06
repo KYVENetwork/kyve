2e07ef9605a31bdeb8e441727d60471670681323
Fabian Riewe <fabianriewe00@gmail.com>
2021-07-06 17:22:55 +0200

    chore: fmt

--- common/query/__tests__/smartweave.spec.ts
+++ common/query/__tests__/smartweave.spec.ts
@@ -1,11 +1,11 @@
-import {readContract} from "../src";
+import { readContract } from "../src";
 
 const chai = require("chai");
 const chaiAsPromised = require("chai-as-promised");
 
 chai.use(chaiAsPromised);
 
-export const {assert, expect} = chai;
+export const { assert, expect } = chai;
 
 describe("Test SmartWeave", () => {
   describe("Getting the latest state", async () => {
