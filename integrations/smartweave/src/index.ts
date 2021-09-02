3060e9683bb4a58cc01d19fef75ffd4f1fd9efe1
John Letey <johnletey@gmail.com>
2021-09-02 11:28:15 +0100

    feat: switch to v2 smartweave

--- integrations/smartweave/src/index.ts
+++ integrations/smartweave/src/index.ts
@@ -5,12 +5,13 @@ import {
   ValidateFunctionSubscriber,
 } from "@kyve/core/dist/faces";
 import Arweave from "arweave";
-import { SwClientFactory } from "smartweave/lib/v2";
 import hash from "object-hash";
 import KYVE from "@kyve/core";
 import Log from "@kyve/core/dist/logger";
 import { JWKInterface } from "arweave/node/lib/wallet";
 import { GQLTagInterface } from "smartweave/lib/interfaces/gqlResult";
+import { SmartWeaveNodeFactory } from "redstone-smartweave";
+import WebSocket from "ws";
 
 const inst = new Arweave({
   host: "arweave.net",
@@ -18,72 +19,50 @@ const inst = new Arweave({
   protocol: "https",
 });
 
-const client = SwClientFactory.memCacheClient(inst);
+const swcClient = SmartWeaveNodeFactory.memCached(inst);
 
 const logger = new Log("SmartWeave");
 
 export const upload = async (
   uploader: UploadFunctionSubscriber,
   pool: string,
-  config: any
+  config: { contracts: string[] }
 ) => {
-  // mapping contract address to hash(state)
-  let contracts: { [id: string]: string } = {};
+  const storage: { [id: string]: string } = {};
 
-  const main = async (previousHeight: number) => {
-    const currentHeight = (await inst.network.getInfo()).height;
-    logger.info(
-      `Current-Height: ${currentHeight} - Previous-Height: ${previousHeight}`
-    );
+  const client = new WebSocket("wss://arweave.ws/blocks");
 
-    if (previousHeight !== currentHeight) {
-      for (const id of config.contracts) {
-        const state = await client.readState(id, currentHeight, undefined, {
-          ignoreExceptions: true,
-        });
+  client.on("message", async (msg) => {
+    const height = parseInt(msg.toString());
 
-        // stringify result
-        const res = JSON.stringify(state);
+    for (const id of config.contracts) {
+      const res = await swcClient.contract(id).readState(height);
+      const resHash = hash(res);
 
-        // if no hash in local storage, upload a new state
-        if (contracts[id]) {
-          const previousHash = contracts[id];
-          const currentHash = hash(res);
+      if (storage[id] === resHash) continue;
 
-          if (previousHash === currentHash) {
-            // no change, can continue
-            continue;
-          }
-        }
+      logger.info("Contract changed, uploading new result...");
+      uploader.next({
+        data: JSON.stringify(res),
+        tags: [
+          { name: "Target-Contract", value: id },
+          { name: "Block", value: height.toString() },
+          { name: "Content-Type", value: "application/json" },
+        ],
+      });
 
-        logger.info("Contract changed, uploading new result...");
-        uploader.next({
-          data: res,
-          tags: [
-            { name: "Target-Contract", value: id },
-            { name: "Block", value: currentHeight },
-          ],
-        });
-
-        // store hash for local comparison
-        contracts[id] = hash(res);
-      }
+      storage[id] = hash(res);
     }
+  });
 
-    //refetch every 10 minutes
-    setTimeout(main, 10 * 60 * 1000, currentHeight);
-  };
-
-  // start with latest block height
-  const height = (await inst.network.getInfo()).height;
-  main(height);
+  client.on("ping", client.pong);
 };
 
 export const validate = async (
   listener: ListenFunctionObservable,
   validator: ValidateFunctionSubscriber,
   pool: string,
-  config: any
+  config: { contracts: string[] }
 ) => {
   listener.subscribe(async (res: ListenFunctionReturn) => {
     // find Target-Contract and Block in transaction tags
@@ -102,9 +81,7 @@ export const validate = async (
     }
 
     // read the contract to the height passed by the uploader
-    const state = await client.readState(contract, block, undefined, {
-      ignoreExceptions: true,
-    });
+    const state = await swcClient.contract(contract).readState(block);
 
     const localHash = hash(JSON.parse(JSON.stringify(state)));
     const compareHash = hash(JSON.parse(res.data));
